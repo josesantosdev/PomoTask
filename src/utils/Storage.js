fb305d2995156a1be2451b561e618f8962cfedec
José Santos <thecaasantos@gmail.com>
2023-10-12 15:25:13 -0300

    :sparkles: feat: armazenamento de dados no dispositivo com asyncstorage

---
+++ src/utils/Storage.js
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const Storage = {
+    setItem:  async (key, value) => {
+        try {
+            await AsyncStorage.setItem(key, value);
+        } catch (error) {
+            console.error('Error setting item in storage:', error);
+        }
+    },
+
+    getItem: async (key) => {
+        try {
+            return await AsyncStorage.getItem(key);
+        } catch (error) {
+            console.error('Error getting item in storage', error);
+            return null;
+        }
+    },
+
+    removeItem: async (key) => {
+        try {
+
+        } catch (error) {
+            console.error('Error removing item in storage', error)
+        }
+    }
+};
