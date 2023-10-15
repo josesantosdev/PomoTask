4a54647e767b30fc4d29062b68168ef68b0b9450
José Santos <thecaasantos@gmail.com>
2023-10-14 23:53:27 -0300

    :sparkles: feat: screens boilerplate components

---
+++ src/screens/CreateTaskScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+
+
+const CreateTaskScreen = () => {
+ 
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Create Task screen its works</Text>
+    </View>
+  );
+};
+
+export default CreateTaskScreen;
---
+++ src/screens/TimerScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+
+
+const TimerScreen = () => {
+ 
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Timer screen its works!</Text>
+    </View>
+  );
+};
+
+export default TimerScreen;
