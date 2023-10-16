8d608acfcedfee662e9e3bf966af5d3359ed1b3c
José Santos <thecaasantos@gmail.com>
2023-10-16 08:41:55 -0300

    :package: build: tailwindcss and nativewinds dependency

--- src/screens/CreateTaskScreen.js
+++ src/screens/CreateTaskScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-paper';
+import { SafeAreaView } from 'react-native';
+import { styled } from 'nativewind';
+import { Text } from 'react-native';
+import { Appbar } from 'react-native-paper';
 
+
+const StyledAppbar = styled(Text)
 
 const CreateTaskScreen = () => {
- 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Create Task screen its works</Text>
-    </View>
+  <SafeAreaView>
+    <Appbar.Header>
+      <Text  
+      className=" font-bold ml-7 text-5xl text-blue-600"
+      >
+        Today
+      </Text>
+    </Appbar.Header>
+  </SafeAreaView>
   );
 };
 
