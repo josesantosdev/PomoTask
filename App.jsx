4a481038e3151d482daa9c5dd65250837624e09b
José Santos <thecaasantos@gmail.com>
2023-12-28 00:18:55 -0300

    feat: componentização do app.jsx e melhoria na legibilidade
    	- Componentizou o arquivo app.jsx para melhorar a organização e
    	legibilidade do código.
    	- Adicionou importações necessárias.
    	- Atualizou estilos do BottomTabNavigator para melhorar a aparência.

--- App.jsx
+++ App.jsx
@@ -1,98 +1,12 @@
 import React from "react";
 import { Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { CardStyleInterpolators } from '@react-navigation/stack'
-import HomeScreen from "./src/screens/HomeScreen";
-import CreateTaskScreen from "./src/screens/CreateTaskScreen";
-import TimerScreen from "./src/screens/TimerScreen";
-import { Entypo, FontAwesome, Ionicons } from '@expo/vector-icons';
 import { StatusBar } from "expo-status-bar";
-import AdminScrean from "./src/screens/AdminScreen";
-
-
-const Tab = createBottomTabNavigator();
-
-
+import Routes from './src/routes';
 export default function App() {
 
   return (
     <PaperProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            tabBarStyle: {
-              backgroundColor: 'rgb(37 99 235)',
-              paddingTop: 10,
-            }
-          }}
-        >
-          <Tab.Screen
-            name="Cadastro de Tarefas"
-            component={CreateTaskScreen}
-            options={{
-              tabBarLabel: 'Tasks',
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesome name="tasks" size={24} color="white" />
-              ),
-              tabBarActiveTintColor: "white",
-              tabBarInactiveTintColor: "#D9D9D6",
-              tabBarLabelStyle: {
-                fontSize: 14,
-                margin: 0,
-              },
-            }}
-          />
-          <Tab.Screen
-            name="Sua Lista de Tarefas"
-            component={HomeScreen}
-            options={{
-              tabBarLabel: 'Home',
-              tabBarIcon: ({ color, size }) => (
-                <Entypo name="home" size={24} color="white" />
-              ),
-              tabBarActiveTintColor: "white",
-              tabBarInactiveTintColor: "#D9D9D6",
-              tabBarLabelStyle: {
-                fontSize: 14,
-                margin: 0,
-              },
-            }}
-          />
-          <Tab.Screen
-            name="Pomodoro"
-            component={TimerScreen}
-            options={{
-              tabBarLabel: 'Pomodoro',
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="time-sharp" size={24} color="white" />
-              ),
-              tabBarActiveTintColor: "white",
-              tabBarInactiveTintColor: "#D9D9D6",
-              tabBarLabelStyle: {
-                fontSize: 14,
-                margin: 0,
-              },
-            }}
-          />
-          <Tab.Screen
-            name="Admin"
-            component={AdminScrean}
-            options={{
-              tabBarLabel: 'Config',
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="cog-sharp" size={24} color="white" />
-              ),
-              tabBarActiveTintColor: "white",
-              tabBarInactiveTintColor: "#D9D9D6",
-              tabBarLabelStyle: {
-                fontSize: 14,
-                margin: 0,
-              },
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <Routes />
       <StatusBar />
     </PaperProvider>
   );
